2d5eb1a0142da4b3e55208ba31c566584ef7e39c
Sumant Kumar <sumant.nitmz94@gmail.com>
2024-01-25 20:05:15 +0100

    first commit to add cypress framework

---
+++ cypress/e2e/homePagenavigation.cy.js
@@ -0,0 +1,6 @@
+describe('Home Page Navigation', () => {
+  it('should verify the Services and Automation are selected when navigating to "Services" and click on "Automation" Sub link', () => {
+    cy.visit('https://www.sogeti.com/')
+  })
+  
+})
